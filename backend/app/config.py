4617090178b60793f4166a1ad1328097ba3cdc46
Rudraksh121a <141481238+Rudraksh121a@users.noreply.github.com>
2025-04-04 19:00:21 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,4 @@
 from dotenv import load_dotenv
 import os
 load_dotenv()
-# Set environment variables (Use dotenv in production)
-USER = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-DBNAME = os.getenv("DBNAME")
+DATABASE_URL = os.getenv("DATABASE_URL")
